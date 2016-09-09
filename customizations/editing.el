5433eabcc5aecaad891c95ee49372e685d110aa1
Justin Sung <justin.sung@greenhouse.io>
2016-09-09 13:12:54 -0400

    some shell changes and required evil-mode

--- customizations/editing.el
+++ customizations/editing.el
@@ -81,3 +81,4 @@
 
 (setq electric-indent-mode nil)
 
+(require 'evil)
--- customizations/shell-integration.el
+++ customizations/shell-integration.el
@@ -4,3 +4,21 @@
   (exec-path-from-shell-initialize)
   (exec-path-from-shell-copy-envs
    '("PATH")))
+
+;; setup multi-term
+(require 'multi-term)
+(setq multi-term-program "/bin/zsh")
+
+;; Set lines kept to 10000
+(add-hook 'term-mode-hook
+          (lambda ()
+            (setq term-buffer-maximum-size 10000)))
+
+(add-hook 'term-mode-hook
+          (lambda ()
+            (add-to-list 'term-bind-key-alist '("s-{" . multi-term-prev))
+            (add-to-list 'term-bind-key-alist '("s-}" . multi-term-next))))
+
+(add-hook 'term-mode-hook
+          (lambda ()
+            (define-key term-raw-map (kbd "C-y") 'term-paste)))
